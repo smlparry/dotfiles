b1d6c5b8e95ac9451cad2a52e79b5c2d571e28e9
smlparry <samuel.parry@bigpond.com>
2015-08-12 20:50:52 +1000

    Add my aliases

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -1,3 +1,14 @@
 alias gaa='git add .; git status; echo "######## MAKE A BRANCH #########"'
 alias commit='git commit -m'
 alias gsp='git smart-pull'
+
+alias showFiles="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
+alias showFiles="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
+alias sshkey="cat ~/.ssh/id_rsa.pub | pbcopy"
+
+alias vu="vagrant up"
+alias vs="vagrant ssh"
+alias vss="vagrant ssh; cd /vagrant/${PWD##*/}"
+alias vh="vagrant halt"
+alias vp="vagrant provision"
+alias vd="vagrant destroy"
