6489ba7493fbed8fe036653863c330defbc64136
smlparry <samuel@zaprri.com>
2018-10-14 18:29:03 -0500

    Get working on new mac

--- shell/zsh/func.zsh
+++ shell/zsh/func.zsh
@@ -2,16 +2,3 @@
 function df-deploy() {
   ./scripts/deploy.sh "$@"
 }
-
-function df-deploy-func() {
-  ./scripts/deploy-func.sh "$@"
-}
-
-function df-invoke() {
-  ./scripts/invoke.sh "$@"
-}
-
-function df-invoke-func() {
-  ./scripts/invoke-func.sh "$@"
-}
-
