c0daad721fd71b5df305d0a71997659fa4249de6
Samuel Hammond <samparry@Samuels-MacBook-Pro.local>
2021-11-18 22:21:14 +1100

    Remove unused libraries from freshrc

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -1,12 +1,7 @@
 alias gaa='git add .; git status'
-alias commit='git commit -m'
-alias gsp='git smart-pull'
-alias gbk='git buildkite'
 alias gcm="git checkout master"
 alias gr="git reset"
 
-alias showFiles="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
-alias hideFiles="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
 alias sshkey="cat ~/.ssh/id_rsa.pub | pbcopy"
 
 alias be="bundle exec"
@@ -14,10 +9,6 @@ alias be="bundle exec"
 alias vi="vim"
 
 alias gup='git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d && git fetch --prune && git rebase --autostash FETCH_HEAD'
-
-# Daily Fire Alias'
-alias sshdfc="ssh deployer@core.daily-fire.com"
-alias sshdfdb="ssh db_user@128.199.130.48"
 
 # Docker Alias
 alias dc="docker-compose"
--- shell/zsh/func.zsh
+++
@@ -1,8 +0,0 @@
-# Daily Fire Functions
-function df-deploy() {
-  ./scripts/deploy.sh "$@"
-}
-
-function df-test() {
-  ./scripts/test.sh "$@"
-}
